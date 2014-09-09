b8fd8acd09fe150c93dcbf6f909ac72307645677
Stephanie <Stephanie@128.61.65.111>
2014-09-09 17:46:02 -0400

    Made simple start screen with title and two buttons

--- SpaceTrader/src/spacetrader/SpaceTrader.java
+++ SpaceTrader/src/spacetrader/SpaceTrader.java
@@ -9,9 +9,16 @@ package spacetrader;
 import javafx.application.Application;
 import javafx.event.ActionEvent;
 import javafx.event.EventHandler;
+import javafx.geometry.Insets;
+import javafx.geometry.Pos;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
+import javafx.scene.layout.GridPane;
+import javafx.scene.layout.HBox;
 import javafx.scene.layout.StackPane;
+import javafx.scene.text.Font;
+import javafx.scene.text.FontWeight;
+import javafx.scene.text.Text;
 import javafx.stage.Stage;
 
 /**
@@ -21,23 +28,43 @@ import javafx.stage.Stage;
 public class SpaceTrader extends Application {
     
     @Override
-    public void start(Stage primaryStage) {
-        Button btn = new Button();
-        btn.setText("Say 'Hello World'");
-        btn.setOnAction(new EventHandler<ActionEvent>() {
-            
+    public void start(Stage primaryStage) {        
+        primaryStage.setTitle("Space Trader");
+        GridPane grid = new GridPane();
+        grid.setAlignment(Pos.CENTER);
+        grid.setHgap(10);
+        grid.setVgap(10);
+        grid.setPadding(new Insets(25, 25, 25, 25));
+        //Make the title text
+        Text scenetitle = new Text("Space Trader");
+        scenetitle.setFont(Font.font("Tahoma", FontWeight.NORMAL, 30));
+        grid.add(scenetitle, 0, 0, 2, 1);
+        //Make the start button and hBox to put buttons in
+        Button startBtn = new Button("New Game");
+        HBox hbBtn = new HBox(10);
+        hbBtn.setAlignment(Pos.BOTTOM_RIGHT);
+        hbBtn.getChildren().add(startBtn);
+        grid.add(hbBtn, 1, 4);
+        startBtn.setOnAction(new EventHandler<ActionEvent>() {
             @Override
             public void handle(ActionEvent event) {
-                System.out.println("Hello World!");
+                System.out.println("Pressed 'New Game'");
+                //move to character config screen
             }
         });
-        
-        StackPane root = new StackPane();
-        root.getChildren().add(btn);
-        
-        Scene scene = new Scene(root, 300, 250);
-        
-        primaryStage.setTitle("Hello World!");
+        //Make the load button
+        Button loadBtn = new Button("Load Game");
+        hbBtn.setAlignment(Pos.BOTTOM_RIGHT);
+        hbBtn.getChildren().add(loadBtn);
+        loadBtn.setOnAction(new EventHandler<ActionEvent>() {
+            @Override
+            public void handle(ActionEvent event) {
+                System.out.println("Pressed 'Load Game'");
+                //move to load game screen
+            }
+        });
+
+        Scene scene = new Scene(grid, 300, 275);
         primaryStage.setScene(scene);
         primaryStage.show();
     }
