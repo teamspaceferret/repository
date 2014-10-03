200a9bc9b0d027e77f16f3e737b0d3870f47b5d2
Cora Wilson <coratatum@gmail.com>
2014-10-03 10:53:55 -0400

    Update sound manager

--- SpaceTrader/src/spacetrader/SoundManager.java
+++ SpaceTrader/src/spacetrader/SoundManager.java
@@ -18,25 +18,85 @@ public class SoundManager {
     private HashMap<String,AudioClip> backgroundMusicMap = new HashMap();
     
     private ExecutorService soundPool;
+    private double volumeBG;
+    private double volumeSE;
     
     public SoundManager(){
         soundPool = Executors.newFixedThreadPool(6);
+        volumeBG = 1;
+        volumeSE = 1;
     }
     
     public SoundManager(int threadNumber){
         soundPool = Executors.newFixedThreadPool(threadNumber);
+        volumeBG = 1;
+        volumeSE = 1;
     }
     
     public void loadSoundEffect(String id, URL url){
         AudioClip soundEffect = new AudioClip(url.toExternalForm());
         soundEffectsMap.put(id, soundEffect);
-        
     }
     
     public void loadBackgroundSound(String id, URL url){
         AudioClip backgroundMusic = new AudioClip(url.toExternalForm());
+        backgroundMusic.setCycleCount(AudioClip.INDEFINITE);
         backgroundMusicMap.put(id, backgroundMusic);
     }
+    
+    public void playSoundEffect(String id){
+        soundEffectsMap.get(id).play(volumeSE);
+    }
+    
+    public void playBackgroundMusic(String id){
+        backgroundMusicMap.get(id).setCycleCount(AudioClip.INDEFINITE);
+        backgroundMusicMap.get(id).play(volumeBG);
+    }
+    
+    public void playBackgroundWithIntro(String introID, String loopID){
+        backgroundMusicMap.get(introID).play(volumeBG);
+        while(backgroundMusicMap.get(introID).isPlaying()){
+            //wait weeeee
+        }
+        playBackgroundMusic(loopID);   
+    }
+    
+    /*
+    public void muteBG(){
+        //loop through all BG and do not play??
+    }
+    
+    public void muteSE(){
+        //loop through all SE and do not play??
+    }
+    */
+    
+    /*
+    public void setVolumeBG(double volume){
+        volumeBG = volume;
+        if (volume==0){
+            muteBG();
+        } else {
+            
+        }
+        //loop through all music
+        //bgmusic.stop
+        //bgmusic.play
+        //replay with current volume
+    }
+    
+    public void setVolumeSE(double volume){
+        volumeSE = volume;
+        
+    }
+    
+    public void setAllVolume(double volume){
+        setVolumeBG(volume);
+        setVolumeSE(volume);
+    }
+    */
+    
+    
     
     
     
